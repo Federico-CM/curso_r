160bfa1b6e942dc919b3930afd86e96ad55dfda4
Federico-CM <fcastro.biol@gmail.com>
2025-12-04 11:27:46 -0600

    Cleaned ejercicios_clase1.R

--- ejercicios_clase1.R
+++ ejercicios_clase1.R
@@ -1,33 +1,32 @@
 # EJERCICIOS BASICOS CLASE 1 #
 
-# Trate de entender que hace cada linea
-# Antes de ejecutar cada linea trate de predecir lo que verá al ejecutarla
-# Añada notas después de cada linea, las notas comienzan con el símbolo #
-# Para ejecutar una linea en la cual está el cursor, apriete "control + enter"
-# Si encuentra un error al ejecutar una linea trate de entender cual es el error
+# Trate de entender qué hace cada línea.
+# Antes de ejecutar cada línea, trate de predecir lo que verá al ejecutarla.
+# Añada notas después de cada línea; las notas comienzan con el símbolo #
+# Para ejecutar una línea en la cual está el cursor, apriete "Ctrl + Enter"
+# Si encuentra un error al ejecutar una línea, trate de entender cuál es el error.
 
+### OPERADORES BÁSICOS ###
 
-### OPERADORES BASICOS ###
+2 + 2              # Ejemplo: esto suma 2 y 2
 
-2+2
+"banana" + 2       # ¿Qué pasa aquí?
 
-"banana" + 2
+2 - 2
 
-2-2
+2 / 2
 
-2/2
+2 ^ 2
 
-2^2
-
-2^3
+2 ^ 3
 
 1:5
 
-1:5+1
+1:5 + 1
 
-1:3^2
+1:3 ^ 2            # ¡Ojo con el orden de las operaciones!
 
-(1:3)^2
+(1:3) ^ 2          # Compare con la línea anterior
 
 2 == 2
 
@@ -41,19 +40,22 @@
 
 2 == 1:3
 
+
 ### CARACTERES ###
 
 "Esteban"
 
-"Esteban"+2
+"Esteban" + 2
 
-"8"+2
+"8" + 2
 
-8+2
+8 + 2
 
 "Esteban" == "Esteban"
 
+
 ### VARIABLES ###
+
 amigo
 
 amigo <- "Esteban"
@@ -62,9 +64,10 @@ amigo
 
 puntos
 
-puntos<-10
+puntos <- 10
 
 puntos
+
 
 ### VECTORES ###
 
@@ -72,7 +75,7 @@ c("Esteban","Laura","Diego")
 
 alumno
 
-alumno<-c("Esteban","Laura","Diego")
+alumno <-c("Esteban","Laura","Diego")
 
 alumno
 
@@ -86,11 +89,11 @@ calificacion
 
 data.frame(alumno,calificacion)
 
-mi_lista
+mi_tabla
 
-mi_lista <- data.frame(alumnos,calificaciones)
+mi_tabla <- data.frame(alumno,calificacion)
 
-### SUBSELECCION ###
+### SUBSELECCIÓN ###
 
 alumno
 
@@ -104,22 +107,32 @@ alumno[2:3]
 
 alumno[1:6]
 
-mi_lista
+mi_tabla
 
-mi_lista[1,]
+mi_tabla[1, ]
 
-mi_lista[2,]
+mi_tabla[2, ]
 
-mi_lista[,1]
+mi_tabla[, 1]
 
-mi_lista[,2]
+mi_tabla[, 2]
 
-mi_lista[1:2,]
+mi_tabla[1:2, ]
 
-mi_lista[2:3,]
+mi_tabla[2:3, ]
 
-mi_lista[,1:2]
+mi_tabla[, 1:2]
 
-mi_lista[1,1]
+mi_tabla[1, 1]
 
-mi_lista[2,1]
+mi_tabla[2, 1]
+
+# terminemos los ejercicios viendo datos interesantes
+# R viene integrado con varios datasets
+?volcano
+
+# frecuentemente hay ejemplos con visualizaciones
+# más adelante haremos visualizaciones similares
+example(volcano)
+
+
